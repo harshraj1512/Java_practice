f2c807d9a0e63ba671d2978d798b8836bdc0790f
Harsh Raj <80125708+harshraj1512@users.noreply.github.com>
2023-06-02 23:12:44 +0530

    Add files via upload

---
+++ loop1/Basics.java
@@ -0,0 +1,30 @@
+package com.loop1;
+
+public class Basics {
+    public static void main(String[] args) {
+      //  int a = 10;
+        //if (a == 10){
+          //  System.out.println("HEllow world!!");
+        //}
+
+        //int count = 1;
+        //while(count != 5){
+          //  System.out.println(count);
+            //count++;
+        //}
+
+        //for(int count = 1; count != 5; count++){
+          //  System.out.println(count);
+        //}
+
+            int a = 2;
+            int b = 0;
+            int result;
+            for(int i = 1; i <= 10; i++){
+
+               // a * i = result;
+            }
+
+
+    }
+}
